6873d70b73933434548c8cf3ee772f579322bac6
Михаил Новиков <spore125@gmail.com>
2023-02-09 14:59:14 +0300

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String surname;
+    public String name;
+    public String patronymic;
+    public String phone;
+    public String passport;
+    public boolean subscribe;
+    public FormDate birthday;
+}
